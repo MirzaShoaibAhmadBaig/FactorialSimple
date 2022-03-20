4f2bc1a04b18e16e78befe726af00254b682774c
armanahmd <92032906+armanahmd@users.noreply.github.com>
2022-03-20 22:16:13 +0500

    Add project files.

---
+++ FactorialSimple/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FactorialSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Number");
+            int Usernum = Int32.Parse(Console.ReadLine());
+            int Factnum = 1;
+            for(int i=1; i<=Usernum; i++)
+            {
+
+
+
+                Factnum = Factnum * i;
+
+
+
+            }
+            Console.WriteLine("Your Provided  Factorial number {0}, and its Factorial is {1}", Usernum, Factnum);
+
+
+        }
+    }
+}
